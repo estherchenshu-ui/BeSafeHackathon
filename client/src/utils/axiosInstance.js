b8165aea73e6ee691beae9c667505aacec47d107
saralemerz <saralemerz@Gmail.com>
2026-01-12 10:23:31 +0200

    Updated history page filters

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_SERVER_API_URL || 'http://localhost:5000/api';
+// const apiUrl = import.meta.env.VITE_SERVER_API_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000/api', 
